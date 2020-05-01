375086f4ed3aff77ae763fadd70f3dd8626446c6
TVSSSRIPAD <tss11@iitbbs.ac.in>
2020-05-01 23:14:03 +0530

    changed stylings and added bootstrap

--- js/baseHTML.js
+++ js/baseHTML.js
@@ -1,14 +1,27 @@
-var base = '<p id="0" style="display: none;"><b>Location: </b></p> \
+var base = '<br><div class="row"> \
+            <div class="col"> \
+            <p id="0" style="display: none;"><b>Location: </b></p> \
+            </div><div class="col"> \
             <p id="1" style="display: none;"><b>Air Qulaity index: </b></p> \
+            </div>\
+            <p style="display: none;" id="ctext"></p>\
+            <div id="circle" style="display: none;">\
+            </div>\
+            </div>\
+            <div class="row"> \
+            <div class="col col-md-3"> \
             <p id="2" style="display: none;"><b>PM 2.5: </b></p> \
             <p id="3" style="display: none;"><b>PM 10: </b></p> \
             <p id="4" style="display: none;"><b>Ozone: </b></p> \
+            </div><div class="col col-md-3"> \
             <p id="5" style="display: none;"><b>Nitigen-di-Oxide: </b></p> \
             <p id="6" style="display: none;"><b>Sulphur-di-Oxide: </b></p> \
             <p id="7" style="display: none;"><b>Carbon Monoxide: </b></p>  \
+            </div><div class="col col-md-3"> \
             <p id="8" style="display: none;"><b>Temperature: </b></p> \
             <p id="9" style="display: none;"><b>Wind: </b></p> \
             <p id="10" style="display: none;"><b>Rain (Precipitation): </b></p> \
+            </div><div class="col col-md-3"> \
             <p id="11" style="display: none;"><b>Relative Humidity: </b></p> \
             <p id="12" style="display: none;"><b>Dew: </b></p> \
-            <p id="13" style="display: none;"><b>Atmospheric Pressure: </b></p>';
+            <p id="13" style="display: none;"><b>Atmospheric Pressure: </b></p></div></div>';
